eb97cef1b6f17d42fec4cada6f58d344388ee5d6
Chcalvanese <ch.calvanese@gmail.com>
2022-09-26 18:02:22 -0300

    CorrelationLog and MatPlotLib

---
+++ Correlation-Log.py
@@ -0,0 +1,10 @@
+# Create log_gdp_per_cap column
+world_happiness['log_gdp_per_cap'] = np.log(world_happiness['gdp_per_cap'])
+
+# Scatterplot of log_gdp_per_cap and happiness_score
+sns.scatterplot(x='log_gdp_per_cap', y='happiness_score', data=world_happiness)
+plt.show()
+
+# Calculate correlation
+cor = world_happiness['log_gdp_per_cap'].corr(world_happiness['happiness_score'])
+print(cor)
