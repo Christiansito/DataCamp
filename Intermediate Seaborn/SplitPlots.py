8ffa690956f661fca51670b7f576d9f7511a8ce7
Chcalvanese <ch.calvanese@gmail.com>
2022-10-20 12:59:03 -0300

    Intermediate Seaborn

---
+++ Intermediate Seaborn/SplitPlots.py
@@ -0,0 +1,23 @@
+# Create an lmplot that has a column for Ownership, a row for Degree_Type and hue based on the WOMENONLY column
+sns.lmplot(data=df,
+        x='SAT_AVG_ALL',
+        y='Tuition',
+        col="Ownership",
+        row='Degree_Type',
+        row_order=['Graduate', 'Bachelors'],
+        hue='WOMENONLY',
+        col_order=inst_ord)
+
+plt.show()
+plt.clf()
+
+# plot relationships between insurance_losses and premiums
+sns.pairplot(data=df,
+             vars=["insurance_losses", "premiums"],
+             kind='reg',
+             palette='BrBG',
+             diag_kind = 'kde',
+             hue='Region')
+
+plt.show()
+plt.clf()
